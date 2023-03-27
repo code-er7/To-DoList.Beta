5d953ea7504f5598321cb07b1a15cd0c5ec1f706
Chetan Rao <chetanrao2003@gmail.com>
2023-03-27 20:59:58 +0530

    new to do list with database

--- app.js
+++ app.js
@@ -1,82 +1,166 @@
 // here after commenting i also have included one more page of /work so the comments are not updated if you want a better hold of how EJS worked check to-do-list-understanding-module in this folder //
 
-
-//requring all  the pacakges we need to make a server work
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 
 //here we are requiring our date module remember that this date varible does not contain a single funtion or value it's an object which can contain as many as function so we can use them later 
 
-
-const date = require(__dirname+'/date.js');
+const date = require(__dirname + '/date.js');
 // console.log(date);
 const app = express();
-app.use(bodyParser.urlencoded({extended : true}));
-app.set("view engine" , "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 // here we telling the server that this public folder will serve as the home of all the static files 
-app.use(express.static("public")); 
-//here we have declared a global variable array of name items so we can send it in the get request of the we can send it in get request of the we browser
-var items = [];
-var WorkItems = [];
-
-//we are responding to the get request coming from the browser
-app.get("/" , function(req , res){
-    //here we created the java script object for telling in what format we want the date 
-    // var options = {
-    //     weekday : "long" , 
-    //     day : "numeric" ,
-    //     month : "long"
-    // };
-    // var today = new Date();
-      
-    //now accesing Date (today) and telling it langauge and format 
-    // var day = today.toLocaleDateString("hi-IN" , options); 
+app.use(express.static("public"));
 
 
+mongoose.connect("mongodb://localhost:27017/todoListDB");
+const itemSchema = {
+    name: String
+};
+const Item = mongoose.model("Item", itemSchema);
+const WorkItem = mongoose.model("WorkItem" , itemSchema);
 
-
-
-    //    here we have made a module ourself named date and in that module we have wrote a function name getDate and we requred that module ( see in top ) and used it's getDate function 
-
-       var day = date.getDate();
-
-    //rendering the things i want to send in the get request 
-    //we are sending the empty array here name item because we cant send it again and again in after every post request the itmes array will update and the updated items array will be gone in the browser
-    res.render("list"  , {ListTittle : day ,  ListItems : items});
-    
+const item1 = new Item({
+    name:"Welcome to To-Do List"
 });
-app.get("/work" , function(req , res){
-    res.render("list" , {ListTittle : "Work List" , ListItems : WorkItems});
+const item2 = new Item({
+    name:"Hit '+' plus to add a new Item"
+});
+const item3 = new Item({
+    name:"<-- Check this to delete a item"
+});
+const defaultItem = [item1 , item2 , item3];
+async function defaultdataInserter(coll)
+{
+    try{
+       await coll.insertMany(defaultItem);
+       console.log("Default Item inserted");
+    }
+    catch(err)
+    {
+      console.log(err);
+    }
+
+}
+async function dataInserter(newItem )
+{
+    try{
+       const itemnew = new Item({
+        name :newItem
+       })
+       await itemnew.save();
+       console.log("New Item inserted");
+    }
+    catch(err)
+    {
+      console.log(err);
+    }
+
+}
+async function dataDeleter(itemid)
+{
+    try{
+      await Item.findByIdAndDelete(itemid);
+      console.log("data deleted succesfully");
+    }
+    catch(err){
+    console.log(err);
+    }
+}
+async function dataDeleter2(itemid)
+{
+    try{
+      let product = await WorkItem.exists({_id: itemid}); 
+    //   console.log(product);
+      await WorkItem.findByIdAndDelete(itemid);
+      console.log("data deleted succesfully");
+      return product;
+    }
+    catch(err){
+    console.log(err);
+    }
+}
+async function dataFinder(route  , res , day , coll){
+    try{
+       const dataitems = await coll.find();
+       if(dataitems.length==0)
+       {
+        defaultdataInserter(coll);
+        res.redirect(route);
+       }
+    //    console.log(data);
+       res.render("list", { ListTittle: day, ListItems: dataitems });
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+}
+app.get("/", function (req, res) {
+    var day = date.getDate();  
+    dataFinder("/" , res , day , Item);
+});
+app.get("/work", function (req, res) {
+    var day = "Work List";
+    dataFinder("/work" , res , day , WorkItem);
 })
-app.get("/about" , function(req , res){
+app.get("/about", function (req, res) {
     res.render("about");
 })
-
-app.post("/" ,  function(req , res ){
-    newItemInTheList = req.body.newItem;
-
-    //here we are just checking from which list post req is coming--- all the requset come to "/" home page but we have dynammicaly given the button it's value so by value we are deciding in which array we have to add new item and which page to render 
-    if(req.body.list === "gotowork")
+app.post("/delete" , function(req , res){
+    // console.log(req.body); 
+    const deleteItem = req.body.checkbox;
+    runi();
+    async function runi()
     {
-        res.redirect("/work"); 
+    let product =  await dataDeleter2(deleteItem);
+    if(product != null)
+    {
+      res.redirect("/work");
     }
-   else if(req.body.list === "gotohome")
+    else
     {
+        dataDeleter(deleteItem);
         res.redirect("/");
-    
     }
-   else if(req.body.list === "Work")
-    {
-        WorkItems.push(newItemInTheList);
+    }
+})
+app.post("/", function (req, res) {
+    newItemInTheList = req.body.newItem;
+    //here we are just checking from which list post req is coming--- all the requset come to "/" home page but we have dynammicaly given the button it's value so by value we are deciding in which array we have to add new item and which page to render 
+    if (req.body.list === "gotowork") {
         res.redirect("/work");
     }
-    else{
-        items.push(newItemInTheList);
+    else if (req.body.list === "gotohome") {
         res.redirect("/");
-    }   
+
+    }
+    else if (req.body.list === "Work") {
+        runnN();
+        async function runnN(){
+        try{
+            const itt = new WorkItem({
+             name :newItemInTheList
+            })
+            await itt.save();
+            console.log("New Item inserted");
+         }
+         catch(err)
+         {
+           console.log(err);
+         }
+        }
+        res.redirect("/work");
+    }
+    else {
+        dataInserter(newItemInTheList )
+        // items.push(newItemInTheList);
+        res.redirect("/");
+    }
 });
-app.listen( 3000 , function(){
+app.listen(3000, function () {
     console.log("Our App is listning at port 3000");
 })
